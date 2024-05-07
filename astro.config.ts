1f091a72f972899555861f4ac5885e0d29040d67
José Ferreira <dev.josejunior@gmail.com>
2024-05-06 23:53:59 -0300

    config google analytics with partytown

--- astro.config.ts
+++ astro.config.ts
@@ -4,8 +4,9 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-
 import { SITE } from "./src/config";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +17,11 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
   ],
   markdown: {
     remarkPlugins: [
